d01ccd1e16a99a31ebc1165a76d21d6cd4534bfb
daman-max <singhdamanjeet86@gmail.com>
2025-08-26 17:49:20 +0530

    LinkedListRevision

---
+++ Arrays/LinkedList/RevisingLinkedList.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    
+    List() {
+        head = NULL;
+        tail = NULL;
+    }
+
+
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = tail = newNode;
+            return;
+        }
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    void pop_front(){
+        if (head == NULL) {
+            cout<<"List is empty"<<endl;
+            return;
+        }
+
+        Node* temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+
+    }
+
+    void pop_back(){
+        if (head == NULL) {
+            cout<<"List is empty"<<endl;
+            return;
+        }
+
+        Node* temp = head;
+        while(temp->next->next !=NULL){
+            temp=temp->next;
+        }
+        temp->next = NULL;
+        delete temp->next;
+
+    }
+
+
+    void printLL() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    List l;
+    l.push_front(10);
+    l.push_front('a');
+    l.push_back(22);
+    l.push_back(25);
+    l.pop_front();
+    l.pop_back();
+    l.printLL();
+    return 0;
+}
